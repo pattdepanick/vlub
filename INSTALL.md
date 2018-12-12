bcc97f026414c955ad4ea06cacfcc35cf01acbe0
Pattdepanick <fpasseron@free.fr>
2018-12-12 12:27:22 +0000

    Various changes:
    - Simplify install instructions with setup script
    - Fix mode for python script
    - everything is used under the vlub dir

--- INSTALL.md
+++ INSTALL.md
@@ -2,9 +2,7 @@
 
 - volumio latest version installed on x86 motherboard
 - LCD usb serial backpack connected
-- pip install python-mpd2
-- pip install pyserial
-- pip install --pre lcdbackpack
+- launch setup.sh
 
 if using python3 (to be done later):
 - pip install pip3
--- setup.sh
+++ setup.sh
@@ -11,11 +11,14 @@ pip install --pre lcdbackpack
 systemctl disable graphical.target
 systemctl set-default multi-user.target
 systemctl start multi-user.target
+# Disable Local Chromium interface for volumio
+systemctl stop volumio-kiosk
+systemctl disable volumio-kiosk
 
 # Setup ROSELCD
-dos2unix /home/volumio/ROSELCDd.py
-chmod 755 /home/volumio/ROSELCDd.py
-ln -sf /home/volumio/volumio-lcd.service /etc/systemd/system
+#dos2unix /home/volumio/vlub/ROSELCDd.py
+chmod 755 /home/volumio/vlub/ROSELCDd.py
+ln -sf /home/volumio/vlub/volumio-lcd.service /etc/systemd/system
 systemctl enable volumio-lcd
 systemctl start volumio-lcd
 #reboot
